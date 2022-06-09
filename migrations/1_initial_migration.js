58f61f1b4027fe368e4720e00f9c192bbbbc69d3
md aamer <mdaamer248@gmail.com>
2022-06-09 17:06:04 +0530

    first commit

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -7,7 +7,11 @@ module.exports = function (deployer) {
   // deployer.deploy(Reward)
   // deployer.deploy(
   //   Vault,
-  //   "",
-  //   ""
+  //   "0x09688a63cCF5E92836035fbBD43A7Ed9F8245718",
+  //   "0x58F5A2Da96629ECdD3e38b55f580Cd8A34beF2B9"
   // )
 }
+
+// NFT Collection   0x09688a63cCF5E92836035fbBD43A7Ed9F8245718
+// Reward           0x58F5A2Da96629ECdD3e38b55f580Cd8A34beF2B9
+// StakingVault     0x5d7dEbc37CbbbFd1364769b5fAd834389FfbC20D
